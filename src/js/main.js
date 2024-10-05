00c04577172143864a6c1606108825c775656f74
ArienI <Laurilin2022@gmail.com>
2024-10-06 00:47:40 +0200

    feat:header languages adaptation

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,17 @@
-import {
-  initSiteSettings
-} from './modules/settings.js';
+import { initSiteSettings } from './modules/settings.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+
   initSiteSettings();
+
+  const dropdownBtn = document.querySelector('.site-header__dropdown-btn');
+  const langList = document.querySelector('.site-header__lang-list');
+
+  if (dropdownBtn && langList) {
+    dropdownBtn.addEventListener('click', () => {
+      langList.classList.toggle('is-visible');
+    });
+  }
 
   window.addEventListener('load', () => {
   });
