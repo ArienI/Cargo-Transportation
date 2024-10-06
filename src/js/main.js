8ee1cac108ba2110e07d25324e565d2192d95d29
ArienI <Laurilin2022@gmail.com>
2024-10-07 00:21:12 +0200

    button language

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,13 @@ document.addEventListener('DOMContentLoaded', () => {
   if (dropdownBtn && langList) {
     dropdownBtn.addEventListener('click', () => {
       langList.classList.toggle('is-visible');
+      event.stopPropagation();
+    });
+
+    document.addEventListener('click', (event) => {
+      if (!dropdownBtn.contains(event.target) && !langList.contains(event.target)) {
+        langList.classList.remove('is-visible');
+      }
     });
   }
 
