2d278878888a909305a3d6ac68c14e3b10752b15
ArienI <Laurilin2022@gmail.com>
2024-10-08 02:35:48 +0200

    fix: nav mobile

--- src/js/modules/lang-dropdown.js
+++ src/js/modules/lang-dropdown.js
@@ -1,17 +1,17 @@
 export const initLangDropdown = () => {
-  const dropdownBtn = document.querySelector( '.site-header__dropdown-btn' );
-  const langList = document.querySelector( '.site-header__lang-list' );
+  const dropdownBtn = document.querySelector('.site-header__dropdown-btn');
+  const langList = document.querySelector('.site-header__lang-list');
 
-  if ( dropdownBtn && langList ) {
-    dropdownBtn.addEventListener( 'click', () => {
-      langList.classList.toggle( 'is-visible' );
+  if (dropdownBtn && langList) {
+    dropdownBtn.addEventListener('click', () => {
+      langList.classList.toggle('is-visible');
       event.stopPropagation();
-    } );
+    });
 
-    document.addEventListener( 'click', ( event ) => {
-      if ( !dropdownBtn.contains( event.target ) && !langList.contains( event.target ) ) {
-        langList.classList.remove( 'is-visible' );
+    document.addEventListener('click', (event) => {
+      if (!dropdownBtn.contains(event.target) && !langList.contains(event.target)) {
+        langList.classList.remove('is-visible');
       }
-    } );
+    });
   }
 };
